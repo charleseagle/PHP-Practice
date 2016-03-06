e49f77bc54ff2c45f9e7a26b79db302a7b48bb7a
Qi Sun (Charles) <sunqicharles@gmail.com>
2016-03-05 19:16:14 -0700

    PHP

---
+++ IncludeRequire/anotherpage.php
@@ -0,0 +1,8 @@
+<?php
+
+include 'header.inc.php';
+
+//require 'header.inc.php';
+
+echo $var2;
+?>
---
+++ IncludeRequire/index.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'header.inc.php';
+
+require 'header.inc.php';
+
+require_once 'header.inc.php';
+
+echo $var1;
+
+?>
+
+
