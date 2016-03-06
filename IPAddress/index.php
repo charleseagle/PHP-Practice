48fce497102c22290ffd7695a14badf32757c466
Qi Sun (Charles) <sunqicharles@gmail.com>
2016-03-05 19:17:58 -0700

    PHP

---
+++ IPAddress/index.php
@@ -0,0 +1,12 @@
+<?php
+require 'conf.inc.php';
+
+foreach ($ip_blocked as $ip) {
+	if ($ip == $ip_address) {
+		die('Your IP address, '.$ip_address.' has been blocked.');
+	}
+}
+
+?>
+
+<h1>Welcome</h1>
